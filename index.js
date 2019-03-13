012763f6f733eca0fead7e0ea6c544e77a8b82c7
Cameron Braid <cameron@drivenow.com.au>
2019-03-13 18:09:30 +1100

    initial import

---
+++ index.js
@@ -0,0 +1,61 @@
+// NodeJs version of the php version
+'use strict';
+
+const Koa = require('koa');
+
+require("marko/src/node-require")
+//require("marko/browser-refresh").enable()
+//require('marko/hot-reload').enable();
+
+process.on('unhandledRejection', error => {
+/* istanbul ignore next */
+  console.error('unhandledRejection', error);
+});
+
+let app = new Koa()
+
+let decorator = require('./decorator.marko');
+let index = require('./index.marko');
+function getTemplate(path) {
+  try {
+    if (path == "/") return index;
+    return require(`.${path }.marko`);
+  }
+  catch (e) {
+    return null
+  }
+}
+
+app.use((ctx, next) => {
+
+  let template = getTemplate(ctx.path)
+  if (template) {
+    return new Promise((resolve, reject)=>{
+      ctx.type = 'html';
+      var stream = decorator.stream({template});
+      stream.on("error", e => {
+        console.error("Error rendering", ctx.request.href)
+        console.error(e)
+        reject()
+      })
+      stream.on("data", data => {
+        if (data !== null) {
+          ctx.res.write(data)
+        }
+      })
+      stream.on("end", () => {
+        ctx.res.end()
+        resolve()
+      })
+    })
+  }
+  else {
+    return send(ctx, ctx.path, { root: __dirname });
+  }
+});
+
+app.listen(process.env.PORT || 8001)
+
+if (process.send) {
+    process.send('online');
+}
